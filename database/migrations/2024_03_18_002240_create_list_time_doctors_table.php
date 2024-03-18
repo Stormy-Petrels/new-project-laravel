b24c39c6e3473aa8bd6473335eb97420ab807c14
MaiXuanNghia <nghiamaiquangtri@gmail.com>
2024-03-18 22:18:32 +0700

    add database EX: php artisan migrate

--- database/migrations/2024_03_18_002240_create_list_time_doctors_table.php
+++ database/migrations/2024_03_18_002240_create_list_time_doctors_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('list_time_doctor', function (Blueprint $table) {
+            $table->string('id');
+            $table->index('id', 'idx_list_time_doctor_id');
+            $table->time('time')->notNull();
+            $table->decimal('price', 10, 2)->nullable();
+            $table->timestamps();
         });
     }
 
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('list_time_doctor');
     }
 };
--- database/migrations/2024_03_18_003638_create_posts_table.php
+++ database/migrations/2024_03_18_003638_create_posts_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id')->primary();
+            $table->string('user_id', 200)->notNull();
+            $table->string('content', 200);
+            $table->string('image', 200)->notNull();
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
