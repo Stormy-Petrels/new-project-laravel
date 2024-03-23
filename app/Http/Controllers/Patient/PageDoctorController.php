c452ccf952654866afec63e9db903ccdf6cf4d78
sinh <sinhdangvan27@gmail.com>
2024-03-23 09:08:35 +0700

    Booking

---
+++ app/Http/Controllers/Patient/PageDoctorController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers\Patient;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\DoctorRepository;
+
+class PageDoctorController extends Controller
+{
+    public function index(){
+        $request = new DoctorRepository;
+        return view('DoctorList',['doctors' => $request->getAllDoctor()]);
+    }
+}
