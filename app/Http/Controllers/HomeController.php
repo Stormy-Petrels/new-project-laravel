9b752829df41f239f1fdf451f7fc1d46446d2faf
Utloan2914 <loan.huynh25@student.passerellesnumeriques.org>
2024-04-16 19:55:33 +0700

    feature/top-doctor

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Banner;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class HomeController extends Controller
 {
     public function index() {
         $banners=Banner::all();
-        return view('patients.home', compact('banners'));
+        $topDoctors = DB::table('doctors')
+        ->join('booking', 'doctors.id', '=', 'booking.doctor_id')
+        ->join('users', 'doctors.user_id', '=', 'users.id')
+        ->select('doctors.id', 'users.name', DB::raw('count(*) as total_bookings'))
+        ->groupBy('doctors.id', 'users.name')
+        ->orderByDesc('total_bookings')
+        ->limit(4)
+        ->get();
+        return view('patients.home', compact('banners'), compact('topDoctors'));
     }
     public function aboutUs() {
         $banners=Banner::all();
